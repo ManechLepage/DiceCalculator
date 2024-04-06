273d21d6eb49a6e302ea0941bbefb31d312df684
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-04-06 01:39:11 -0400

    adsfzxc

--- DiceCalculator/Assets/Scripts/InfoManager.cs
+++ DiceCalculator/Assets/Scripts/InfoManager.cs
@@ -7,15 +7,58 @@ public class InfoManager : MonoBehaviour
     public int chartType;
     public int chartDataType;
     public List<Vector2> plotValues = new List<Vector2>();
+    public List<Vector2> tmpPlotValues = new List<Vector2>();
+
+    public void Awake()
+    {
+        tmpPlotValues = plotValues;
+    }
 
     public void ChangeChartType(int type)
     {
         chartType = type;
+        plotValues = tmpPlotValues;
+        CreateGraph();
     }
 
     public void ChangeChartDataType(int type)
     {
         chartDataType = type;
+        plotValues = tmpPlotValues;
+        if (chartDataType == 0)
+        {
+            plotValues = tmpPlotValues;
+        }
+        else if (chartDataType == 1)
+        {
+            TransformPlotValuesToAtLeast();
+        }
+        else if (chartDataType == 2)
+        {
+            TransformPlotValuesToAtMost();
+        }
+        CreateGraph();
+    }
+
+    public void TransformPlotValuesToAtLeast()
+    {
+        float sum = 1;
+        for (int i = 0; i < plotValues.Count; i++)
+        {
+            float tmpSum = sum;
+            sum -= plotValues[i].y;
+            plotValues[i] = new Vector2(tmpPlotValues[i].x, tmpSum);
+        }
+    }
+
+    public void TransformPlotValuesToAtMost()
+    {
+        float sum = 1;
+        for (int i = plotValues.Count; i < 0; i--)
+        {
+            plotValues[i] = new Vector2(plotValues[i].x, sum);
+            sum += tmpPlotValues[i].y;
+        }
     }
 
 
