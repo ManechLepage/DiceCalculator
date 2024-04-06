c7a92ff2d440998d46fc922b246936524a355316
Renaud Deschenes <deschenesr@educ.college-montreal.qc.ca>
2024-04-06 10:56:10 -0400

    COLORS !!!!!!!!!!!

--- DiceCalculator/Assets/Scripts/ExpressionManager.cs
+++ DiceCalculator/Assets/Scripts/ExpressionManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum ExpressionType
 {
@@ -18,10 +19,33 @@ public class ExpressionManager : MonoBehaviour
     public ExpressionType type;
     public bool hasOperators = true;
     public int maxLength = -1;
+    public Colors colors;
 
     public void Destroy()
     {
         Destroy(gameObject);
         gameObject.transform.parent.GetComponent<PanelManager>().UpdatePanel();
     }
+
+    public void Update()
+    {
+        if (type != ExpressionType.Operator)
+        {
+            gameObject.GetComponent<Image>().color = colors.colors[(CalculateNestingLevel(transform) - 2) % colors.colors.Count];  
+        }
+    }
+
+    int CalculateNestingLevel(Transform currentTransform, int currentLevel = 0)
+    {
+        if (currentTransform.parent == null)
+        {
+            return currentLevel;
+        }
+        else
+        {
+            return CalculateNestingLevel(currentTransform.parent, currentLevel + 1);
+        }
+    }
+
+
 }
